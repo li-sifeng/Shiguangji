c501041a2b680066eca7c78712b8565f543ff1ef
Sifeng Li <sifengli@microsoft.com>
2025-06-01 10:25:49 +0800

    feat: about页面改为静态页面并展示菜单栏

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.send('关于 拾光纪：这是我的个人网站。');
+  res.sendFile(path.join(__dirname, '../public/about.html'));
 });
 
 /* GET blog page. */
@@ -20,6 +20,16 @@ router.get('/blog', function(req, res, next) {
 /* GET tools page. */
 router.get('/tools', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/tools.html'));
+});
+
+/* GET idphoto page. */
+router.get('/idphoto', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/idphoto.html'));
+});
+
+/* GET game page. */
+router.get('/game', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/game.html'));
 });
 
 module.exports = router;
